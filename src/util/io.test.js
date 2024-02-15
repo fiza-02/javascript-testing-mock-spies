4a33ae31fc63830807c1d6080f42eea31dd99476
Mohammed Yasmin <mdyasmin0210@gmail.com>
2024-02-15 14:44:08 +0530

    use of mock for replacing functionality

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -1,8 +1,15 @@
-import { it, expect } from "vitest";
+import { it, expect, vi } from "vitest";
 import writeData from "./io";
+import { promises as fs } from "fs";
+
+// arguments as name of the module or the path of the module that should be mocked
+vi.mock("fs");
+// it will trigger vitest auto-mocking algorithm it will find this module and replace all functions in there with empty spy  functions
 
 it("should execute the writeFile method", () => {
 	const testData = "test";
 	const testFilename = "test.txt";
-	return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+	writeData(testData, testFilename);
+	// return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+	expect(fs.writeFile).toBeCalled();
 });
