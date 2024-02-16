8cbb316ceae1818a166a806d30f6a80c0baedf66
Mohammed Yasmin <mdyasmin0210@gmail.com>
2024-02-16 12:35:51 +0530

    added custom mock logic

--- src/util/io.test.js
+++ src/util/io.test.js
@@ -5,11 +5,24 @@ import { promises as fs } from "fs";
 // arguments as name of the module or the path of the module that should be mocked
 vi.mock("fs");
 // it will trigger vitest auto-mocking algorithm it will find this module and replace all functions in there with empty spy  functions
+// custom mock logic replace the join method in path with our own method since we have imported defualt we have to use default object to define the method
+vi.mock("path", () => {
+	return {
+		default: {
+			join: (...args) => {
+				return args[args.length - 1];
+			},
+		},
+	};
+});
 
 it("should execute the writeFile method", () => {
-	const testData = "test";
+	const testData = "Test";
 	const testFilename = "test.txt";
+
 	writeData(testData, testFilename);
+
 	// return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
-	expect(fs.writeFile).toBeCalled();
+	// expect(fs.writeFile).toBeCalled();
+	expect(fs.writeFile).toBeCalledWith(testFilename, testData);
 });
